3f3a91d4933171c424c9a8b55b98cf00524f5746
prkr-28 <priyanshu402muz@gmail.com>
2025-08-14 17:19:56 +0530

    deployment

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,44 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import connectDB from './config/DB.js';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import authrouter from './routes/auth.js';
-import userRouter from './routes/user.js';
-import postRouter from './routes/post.js';
-import loopRouter from './routes/loop.js';
-import storyRouter from './routes/story.js';
-import messageRouter from './routes/message.js';
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/DB.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import authrouter from "./routes/auth.js";
+import userRouter from "./routes/user.js";
+import postRouter from "./routes/post.js";
+import loopRouter from "./routes/loop.js";
+import storyRouter from "./routes/story.js";
+import messageRouter from "./routes/message.js";
+import path from "path";
 dotenv.config();
-import {app,server} from './socket.js';
+import { app, server } from "./socket.js";
 
 const PORT = process.env.PORT || 4000;
 
+const __dirname = path.resolve();
+
 app.use(
-   cors({
-      origin: 'http://localhost:5173',
-      credentials: true,
-   })
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
 );
 app.use(express.json());
 app.use(cookieParser());
 
-app.use('/api/auth', authrouter);
-app.use('/api/user', userRouter);
-app.use('/api/post', postRouter);
-app.use('/api/loop', loopRouter);
-app.use('/api/story', storyRouter);
-app.use('/api/message', messageRouter);
+app.use("/api/auth", authrouter);
+app.use("/api/user", userRouter);
+app.use("/api/post", postRouter);
+app.use("/api/loop", loopRouter);
+app.use("/api/story", storyRouter);
+app.use("/api/message", messageRouter);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("/*", (_, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
-   connectDB();
-   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
